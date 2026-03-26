21f5cf866797825ce552dfeb24470ff023767cba
AKSHIT GAUTTAM <akshitgauttam@AKSHITs-MacBook-Air-3.local>
2026-03-26 23:08:41 +0530

    Fix API and Socket URL

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,5 @@
 // frontend/src/config/api.js
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5030';
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5030';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://quicknotes-z85o.onrender.com';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://quicknotes-z85o.onrender.com';
 
 export { API_BASE_URL, SOCKET_URL };
